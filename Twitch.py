a73a607c2e5b947017148cdc6bd8b63052469777
Enrique Aleixandre <ealeixandremp@gmail.com>
2021-01-30 20:17:44 +0100

    Starting development

--- Twitch.py
+++ Twitch.py
@@ -1,5 +1,45 @@
 from dotenv import load_dotenv
+import logging
 import os
 import requests
 
 load_dotenv(".env.local")
+
+# Managing everything through a session seems simpler
+session = requests.Session()
+
+# Urls dictionary to easily edit them if necessary
+urls = {
+    "auth": "https://id.twitch.tv/oauth2/token",
+    "streams": "https://api.twitch.tv/helix/streams"
+}
+
+
+def get_access_token():
+    client_id = os.getenv("CLIENTID")
+    data = {
+        "client_id": client_id,
+        "client_secret": os.getenv("SECRET"),
+        "grant_type": "client_credentials"
+    }
+
+    r = session.post(urls["auth"], data=data)
+    response = r.json()
+
+    try:
+        token = response["access_token"]
+        session.headers.update({
+            "client-id": client_id,
+            "Authorization": ("Bearer %s" % token)
+        })
+        logging.info("Authorized correctly")
+    except KeyError:
+        message = response.get("message")
+        if message is not None:
+            logging.critical("Exception when authorizing: %s" % response.get("message"))
+        else:
+            logging.critical("Exception when authorizing. No message returned.")
+
+
+logging.info("Started scrapping")
+get_access_token()
---
+++ main.py
@@ -0,0 +1,6 @@
+# IMPORTS
+import Twitch
+import logging
+
+# Configuring the logger
+logging.basicConfig(filename="log.txt", format="%(asctime)s - %(levelname)s - %(message)s")
