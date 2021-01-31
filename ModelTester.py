7710e1f498bc257cc2d49aebf967fc62c06e0b54
Enrique Aleixandre <enrique.floged@gmail.com>
2021-01-31 21:26:31 +0100

    Adds create_scrap function on Model.py

--- ModelTester.py
+++ ModelTester.py
@@ -1,2 +1,14 @@
 from classes import Model
+import dotenv
+from datetime import datetime, timedelta
+import os
+
+dotenv.load_dotenv(".env.local")
+Model.initialize(os.getenv("DBCON"))
+start = datetime.utcnow() - timedelta(days=1)
+end = datetime.utcnow()
+print(Model.get_scraps(start, end))
+print(Model.create_scrap())
+print(Model.get_scraps(start, datetime.now()))
+Model.close()
 
--- classes/Model.py
+++ classes/Model.py
@@ -1,19 +1,48 @@
 import psycopg2
-import datetime
+import pytz
+from datetime import datetime
 
 __connector = None
-__cursor = None
+
 
 def initialize(constring: str):
     global __connector
     __connector = psycopg2.connect(constring)
 
+
+def __check() -> bool:
+    return __connector is not None
+
+
 def close():
-    if __connector is not None:
+    if __check():
         __connector.close()
 
-def get_scraps(start_date: datetime, end_time: datetime):
-    pass
+
+def get_scraps(start_time: datetime, end_time: datetime):
+    cursor = __connector.cursor()
+    query = "SELECT * FROM scraps WHERE created_at BETWEEN %s AND %s"
+    cursor.execute(query, (start_time, end_time))
+
+    scraps = cursor.fetchall()
+    cursor.close()
+    return scraps
+
+def create_scrap():
+    cursor = __connector.cursor()
+    created_at = datetime.utcnow()
+    query = "INSERT INTO scraps (created_at) values (%s)"
+    cursor.execute(query, (created_at,))
+    __connector.commit()
+    cursor.close()
+
 
 def get_streamers():
-    pass
+    cursor = __connector.cursor()
+    query = "SELECT * FROM streamers"
+    cursor.execute(query)
+
+    streamers = cursor.fetchall()
+    cursor.close()
+
+    return streamers
