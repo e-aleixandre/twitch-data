3f7e8413f58fe3e2a708128335a80779a9e0bcc8
E. Aleixandre <ealeixandremp@gmail.com>
2021-03-09 07:48:29 +0100

    Replaced deleted by accident file

---
+++ server/config/datastores.js
@@ -0,0 +1,11 @@
+
+module.exports.datastores = {
+  default: {
+    //adapter: require('sails-mysql'),
+    //url: 'mysql://root:squ1ddy@localhost:3306/my_dev_db_name',
+  },
+  mongoDatabase: {
+    //adapter: require('sails-mongo'),
+    //url: ''
+  }
+};
